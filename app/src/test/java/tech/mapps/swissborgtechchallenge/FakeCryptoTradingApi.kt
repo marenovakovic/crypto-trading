303ed57fceba1b075b613253a7d68105088049e2
markonovakovic <graornovakovic@gmail.com>
2024-05-27 18:04:38 +0200

    move

--- app/src/test/java/tech/mapps/swissborgtechchallenge/FakeCryptoTradingApi.kt
+++ app/src/test/java/tech/mapps/swissborgtechchallenge/FakeCryptoTradingApi.kt
@@ -3,20 +3,6 @@ package tech.mapps.swissborgtechchallenge
 import tech.mapps.swissborgtechchallenge.api.CryptoTradingApi
 import tech.mapps.swissborgtechchallenge.api.Ticker
 
-data class GenericTicker(
-    override val symbol: String = "",
-    override val priceOfLastHighestBid: Float = 0f,
-    override val sumOf25HighestBids: Float = 0f,
-    override val lastLowestAsk: Float = 0f,
-    override val sumOf25LowestAsks: Float = 0f,
-    override val dailyChange: Float = 0f,
-    override val dailyChangePercentage: Float = 0f,
-    override val lastTradePrice: Float = 0f,
-    override val dailyVolume: Float = 0f,
-    override val dailyHigh: Float = 0f,
-    override val dailyLow: Float = 0f,
-) : Ticker
-
 object FailingCryptoTradingApi : CryptoTradingApi {
     override suspend fun getTickers() = throw Throwable()
 }
--- app/src/test/java/tech/mapps/swissborgtechchallenge/randomTickers.kt
+++ app/src/test/java/tech/mapps/swissborgtechchallenge/randomTickers.kt
@@ -7,3 +7,17 @@ val randomTickers: List<Ticker>
     get() = List(Random.nextInt(1, 5)) {
         GenericTicker(symbol = it.toString())
     }
+
+data class GenericTicker(
+    override val symbol: String = "",
+    override val priceOfLastHighestBid: Float = 0f,
+    override val sumOf25HighestBids: Float = 0f,
+    override val lastLowestAsk: Float = 0f,
+    override val sumOf25LowestAsks: Float = 0f,
+    override val dailyChange: Float = 0f,
+    override val dailyChangePercentage: Float = 0f,
+    override val lastTradePrice: Float = 0f,
+    override val dailyVolume: Float = 0f,
+    override val dailyHigh: Float = 0f,
+    override val dailyLow: Float = 0f,
+) : Ticker
