b2d1e2845e2952476bb464a996c1efff71563d95
markonovakovic <graornovakovic@gmail.com>
2024-05-27 18:04:19 +0200

    delete GetTickers.kt

--- app/src/test/java/tech/mapps/swissborgtechchallenge/FakeCryptoTradingApi.kt
+++ app/src/test/java/tech/mapps/swissborgtechchallenge/FakeCryptoTradingApi.kt
@@ -1,13 +1,7 @@
 package tech.mapps.swissborgtechchallenge
 
-import arrow.core.left
-import arrow.core.right
-import kotlinx.collections.immutable.toImmutableList
-import kotlinx.coroutines.test.runTest
 import tech.mapps.swissborgtechchallenge.api.CryptoTradingApi
 import tech.mapps.swissborgtechchallenge.api.Ticker
-import kotlin.test.Test
-import kotlin.test.assertEquals
 
 data class GenericTicker(
     override val symbol: String = "",
@@ -31,22 +25,4 @@ class FakeCryptoTradingApi : CryptoTradingApi {
     val tickers = List(3) { GenericTicker(symbol = it.toString()) }
 
     override suspend fun getTickers() = tickers
-}
-
-class GetTickersTest {
-
-    @Test
-    fun `return Left when api call fails`() = runTest {
-        val getTickers = GetTickersImpl(FailingCryptoTradingApi)
-
-        assertEquals(Unit.left(), getTickers())
-    }
-
-    @Test
-    fun `return tickers from the api when call succeeds`() = runTest {
-        val api = FakeCryptoTradingApi()
-        val getTickers = GetTickersImpl(api)
-
-        assertEquals(api.tickers.toImmutableList().right(), getTickers())
-    }
 }
