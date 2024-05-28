c697d718fda3048affffda62390746ca640855c3
markonovakovic <graornovakovic@gmail.com>
2024-05-28 15:26:33 +0200

    delete unneeded

--- app/src/main/java/tech/mapps/swissborgtechchallenge/ui/TradingScreen.kt
+++ app/src/main/java/tech/mapps/swissborgtechchallenge/ui/TradingScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -37,7 +38,7 @@ fun TradingScreen(
             CenterAlignedTopAppBar(
                 title = {
                     Column {
-                        TextField(
+                        OutlinedTextField(
                             value = state.query,
                             onValueChange = viewModel::search,
                             modifier = Modifier.fillMaxWidth(),
