2d7f60f7f6a1c84b1bb5fcdd710725dfaae5c9e0
markonovakovic <graornovakovic@gmail.com>
2024-05-28 15:35:02 +0200

    better styling

--- app/src/main/java/tech/mapps/swissborgtechchallenge/ui/TradingScreen.kt
+++ app/src/main/java/tech/mapps/swissborgtechchallenge/ui/TradingScreen.kt
@@ -2,27 +2,31 @@
 
 package tech.mapps.swissborgtechchallenge.ui
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import tech.mapps.swissborgtechchallenge.R
 import tech.mapps.swissborgtechchallenge.TradingViewModel
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun TradingScreen(
     modifier: Modifier = Modifier,
@@ -34,24 +38,20 @@ fun TradingScreen(
 
     Scaffold(
         modifier = modifier.fillMaxSize(),
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    Column {
-                        OutlinedTextField(
-                            value = state.query,
-                            onValueChange = viewModel::search,
-                            modifier = Modifier.fillMaxWidth(),
-                        )
-                    }
-                },
-            )
-        },
         content = { innerPadding ->
             LazyColumn(
                 contentPadding = PaddingValues(8.dp),
                 modifier = Modifier.padding(innerPadding),
             ) {
+                stickyHeader {
+                    OutlinedTextField(
+                        label = { Text(text = stringResource(R.string.ticker)) },
+                        value = state.query,
+                        onValueChange = viewModel::search,
+                        modifier = Modifier.fillMaxWidth(),
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                }
                 items(state.tickers) { ticker ->
                     TickerCard(
                         ticker = ticker,
