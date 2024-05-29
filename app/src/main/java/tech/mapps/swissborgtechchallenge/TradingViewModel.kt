848d808197f6fe79f5611853611db8b9205cfdd9
markonovakovic <graornovakovic@gmail.com>
2024-05-29 11:39:19 +0200

    moving TradingState to a separate file

--- app/src/main/java/tech/mapps/swissborgtechchallenge/TradingViewModel.kt
+++ app/src/main/java/tech/mapps/swissborgtechchallenge/TradingViewModel.kt
@@ -8,7 +8,6 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -20,44 +19,6 @@ import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import tech.mapps.swissborgtechchallenge.TickerSorting.ByNameAscending
-import tech.mapps.swissborgtechchallenge.TickerSorting.ByNameDescending
-import tech.mapps.swissborgtechchallenge.TickerSorting.ByPriceAscending
-import tech.mapps.swissborgtechchallenge.TickerSorting.ByPriceDescending
-
-data class TradingState(
-    private val allTickers: ImmutableList<Ticker> = persistentListOf(),
-    val connectivityStatus: ConnectivityStatus? = null,
-    val tickerSorting: TickerSorting = ByPriceDescending,
-    val query: String = "",
-    val error: Unit? = null,
-) {
-    val isLoading =
-        connectivityStatus != ConnectivityStatus.Unavailable &&
-                error == null &&
-                allTickers.isEmpty() &&
-                query.isBlank()
-
-    val tickers = tickerSorting.sort(allTickers.search(query))
-
-    fun sortByPrice() =
-        copy(
-            tickerSorting =
-            when (this.tickerSorting) {
-                ByPriceDescending -> ByPriceAscending
-                else -> ByPriceDescending
-            },
-        )
-
-    fun sortByName() =
-        copy(
-            tickerSorting =
-            when (this.tickerSorting) {
-                ByNameAscending -> ByNameDescending
-                else -> ByNameAscending
-            },
-        )
-}
 
 @HiltViewModel
 class TradingViewModel @Inject constructor(
