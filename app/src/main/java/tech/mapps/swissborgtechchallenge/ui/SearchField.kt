7305af49df1c615fbac8da7e971537b5e6c37442
markonovakovic <graornovakovic@gmail.com>
2024-05-29 12:27:32 +0200

    SearchField @Preview

--- app/src/main/java/tech/mapps/swissborgtechchallenge/ui/SearchField.kt
+++ app/src/main/java/tech/mapps/swissborgtechchallenge/ui/SearchField.kt
@@ -9,8 +9,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import tech.mapps.swissborgtechchallenge.R
+import tech.mapps.swissborgtechchallenge.ui.theme.SwissborgTechChallengeTheme
 
 @Composable
 fun SearchField(
@@ -26,4 +28,26 @@ fun SearchField(
             .padding(horizontal = 8.dp)
             .background(color = MaterialTheme.colorScheme.background),
     )
+}
+
+@Preview
+@Composable
+private fun SearchFieldEmptyPreview() {
+    SwissborgTechChallengeTheme {
+        SearchField(
+            query = "",
+            onQueryChange = {},
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun SearchFieldFilledInPreview() {
+    SwissborgTechChallengeTheme {
+        SearchField(
+            query = "BTC",
+            onQueryChange = {},
+        )
+    }
 }
