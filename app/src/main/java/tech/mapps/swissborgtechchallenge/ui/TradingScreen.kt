bfbd3742824a89b8fb695316222af7ac81cd2154
markonovakovic <graornovakovic@gmail.com>
2024-05-29 12:42:16 +0200

    window insets bottom padding for error widget so it's fully visible and not hidden by navigation bar

--- app/src/main/java/tech/mapps/swissborgtechchallenge/ui/TradingScreen.kt
+++ app/src/main/java/tech/mapps/swissborgtechchallenge/ui/TradingScreen.kt
@@ -10,9 +10,12 @@ import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -87,7 +90,9 @@ private fun TradingScreenContent(
                     visible = tradingState.connectivityStatus == ConnectivityStatus.Unavailable || tradingState.error != null,
                     enter = slideInVertically { it } + fadeIn(),
                     exit = slideOutVertically { it } + fadeOut(),
-                    modifier = Modifier.align(Alignment.BottomCenter),
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .windowInsetsPadding(WindowInsets.navigationBars),
                 ) {
                     ErrorWidget(tradingState = tradingState)
                 }
